b1ccd8c6cf0fee9a97fd331cf8221cfbb30ccc39
Imrane ALI LAFKIH <imrane.laf@gmail.com>
2023-06-06 13:21:33 -0700

    00 to 99

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
-
 for n in range(0, 100):
-    if n <10:
-        print("0{}".format(n), end=", ") 
+    if n < 10:
+        print("0{}".format(n), end=", ")
     else:
         print("{}".format(n), end=", ")
