62bec5f9f909e522157519ed265cbafdcc7f6b77
Imrane ALI LAFKIH <imrane.laf@gmail.com>
2023-06-06 15:39:18 -0700

    Remove at position

--- 0x01-python-if_else_loops_functions/101-remove_char_at.py
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 def remove_char_at(str, n):
     if n < 0:
-        return str # If n is negative, return the original string unchanged
-    return str[:n] + str[n+1:]  # Concatenate the parts of the string before and after the character at index n
+        # If n is negative, return the original string unchanged
+        return str
+    # Concatenate the parts of the string before and after the character at index n
+    return str[:n] + str[n+1:] 
