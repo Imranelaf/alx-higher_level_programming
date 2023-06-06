edd63d2447b3c5aef34c985558fa721a82eeb95e
Imrane ALI LAFKIH <imrane.laf@gmail.com>
2023-06-06 15:30:05 -0700

    Smile in the mirror

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
-case=0
+case = 0
 for letter in range(ord('z'), ord('a') - 1, -1):
     print("{}".format(chr(letter - case)), end="")
-    if case==0:
-        case=32
+    if case == 0:
+        case = 32
     else:
-        case=0
+        case = 0
