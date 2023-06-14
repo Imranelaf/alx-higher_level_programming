c00206da0681fcea0234043c79123a78c7b6a550
Imrane ALI LAFKIH <imrane.laf@gmail.com>
2023-06-14 09:26:53 -0700

    Search and replace

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,5 +1,3 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
-    new_list = my_list.copy()
-    new_list[search] = replace
-    return new_list
+   return [replace if n == search else n for n in my_list]
