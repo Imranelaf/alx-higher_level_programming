8e22af99a3991b48dbaf3907ac75fbeee26e3e66
Imrane ALI LAFKIH <imrane.laf@gmail.com>
2023-06-26 08:03:08 -0700

    Safe list printing

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -3,7 +3,7 @@ def safe_print_list(my_list=[], x=0):
     for i in my_list:
         try:
             if count < x:
-                print(i, end="")
+                print("{}".format(my_list[i]), end="")
                 count += 1
             else:
                 break
