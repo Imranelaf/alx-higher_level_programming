9155a139ad71802b4c464a80b3c1b7b48c88c08b
Imrane ALI LAFKIH <imrane.laf@gmail.com>
2023-07-21 13:42:48 -0700

    Python - Almost a circle

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -1,102 +1,69 @@
 #!/usr/bin/python3
-"""Defines a rectangle class."""
 from models.base import Base
 
-
 class Rectangle(Base):
-   """Initialize a new Rectangle."""
+    """Rectangle class that inherits from Base"""
 
     def __init__(self, width, height, x=0, y=0, id=None):
+        """Initialize the Rectangle instance"""
+        super().__init__(id)
         self.width = width
         self.height = height
         self.x = x
         self.y = y
-        super().__init__(id)
 
     @property
     def width(self):
-        """Set/get the width of the Rectangle."""
+        """Getter method for width"""
         return self.__width
 
     @width.setter
     def width(self, value):
-        self.setter_validation("width", value)
+        """Setter method for width"""
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value <= 0:
+            raise ValueError("width must be greater than 0")
         self.__width = value
 
     @property
     def height(self):
-       """the height of the Rectangle."""
+        """Getter method for height"""
         return self.__height
 
     @height.setter
     def height(self, value):
-        self.setter_validation("height", value)
+        """Setter method for height"""
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value <= 0:
+            raise ValueError("height must be greater than 0")
         self.__height = value
 
     @property
     def x(self):
-    """the x coordinate of the Rectangle."""
+        """Getter method for x"""
         return self.__x
 
     @x.setter
     def x(self, value):
-        self.setter_validation("x", value)
+        """Setter method for x"""
+        if not isinstance(value, int):
+            raise TypeError("x must be an integer")
+        if value < 0:
+            raise ValueError("x must be non-negative")
         self.__x = value
 
     @property
     def y(self):
-    """the y coordinate of the Rectangle."""
+        """Getter method for y"""
         return self.__y
 
     @y.setter
     def y(self, value):
-        self.setter_validation("y", value)
+        """Setter method for y"""
+        if not isinstance(value, int):
+            raise TypeError("y must be an integer")
+        if value < 0:
+            raise ValueError("y must be non-negative")
         self.__y = value
-
-    def area(self):
-       """Return the area of the Rectangle."""
-        return (self.height * self.width)
-
-    def display(self):
-        """Print the Rectangle using the shape"""
-        rectangle = ""
-        print("\n" * self.y, end="")
-        for i in range(self.height):
-            rectangle += (" " * self.x) + ("#" * self.width) + "\n"
-        print(rectangle, end="")
-
-    def update(self, *args, **kwargs):
-        """update the Rectangle """
-        if len(args) == 0:
-            for key, val in kwargs.items():
-                self.__setattr__(key, val)
-            return
-        try:
-            self.id = args[0]
-            self.width = args[1]
-            self.height = args[2]
-            self.x = args[3]
-            self.y = args[4]
-        except IndexError:
-            pass
-
-    def to_dictionary(self):
-        return {'x': getattr(self, "x"),
-                'y': getattr(self, "y"),
-                'id': getattr(self, "id"),
-                'height': getattr(self, "height"),
-                'width': getattr(self, "width")}
-
-    @staticmethod
-    def setter_validation(attribute, value):
-        if type(value) != int:
-            raise TypeError("{} must be an integer".format(attribute))
-        if attribute == "x" or attribute == "y":
-            if value < 0:
-                raise ValueError("{} must be >= 0".format(attribute))
-        elif value <= 0:
-            raise ValueError("{} must be > 0".format(attribute))
-
-    def __str__(self):
-        return "[Rectangle] ({}) {}/{} - {}/{}".format(self.id, self.x, self.y,
-                                                       self.width, self.height)
