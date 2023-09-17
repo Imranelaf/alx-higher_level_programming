79b1083568556b270ed273fa3b96d8dc2daeed6f
Imrane ALI LAFKIH <imrane.laf@gmail.com>
2023-09-17 15:37:26 -0700

    All cities by state

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -6,7 +6,6 @@ import MySQLdb
 from sys import argv
 
 if __name__ == '__main__':
-
     database = MySQLdb.connect(host="localhost", user=argv[1], port=3306,
                                passwd=argv[2], db=argv[3])
 
