445a7efe18164f9eef3e6ab1785f46faaf8c571a
Imrane ALI LAFKIH <imrane.laf@gmail.com>
2023-09-17 16:27:48 -0700

    City relationship

--- 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
+++ 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
@@ -1,23 +1,26 @@
 #!/usr/bin/python3
-"""Improve the files model_city.py and model_state.py"""
+"""
+This script connects to a MySQL database, creates a new State object
+(California) and a new City object (San Francisco), establishes a
+relationship between them, and commits the changes to the database.
+"""
+
+from sys import argv
+from relationship_state import Base, State
+from relationship_city import City
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
 
 if __name__ == "__main__":
-
-    import sys
-    from relationship_state import Base, State
-    from relationship_city import City
-    from sqlalchemy import create_engine
-    from sqlalchemy.orm import Session
-    from sqlalchemy.schema import Table
-
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
-                           .format(sys.argv[1], sys.argv[2],
-                                   sys.argv[3]), pool_pre_ping=True)
-    Base.metadata.create_all(engine)
-    session = Session(engine)
-    new_city = City(name='San Francisco')
-    new = State(name='California')
-    new.cities.append(new_city)
-    session.add_all([new, new_city])
-    session.commit()
-    session.close()
+    database = 'mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
+        argv[1], argv[2], argv[3])
+    eng = create_engine(database)
+    Base.metadata.create_all(eng)
+    Session = sessionmaker(bind=eng)
+    sess = Session()
+    cal_state = State(name='California')
+    sfr_city = City(name='San Francisco')
+    cal_state.cities.append(sfr_city)
+    sess.add(cal_state)
+    sess.commit()
+    sess.close()
--- 0x0F-python-object_relational_mapping/relationship_state.py
+++ 0x0F-python-object_relational_mapping/relationship_state.py
@@ -1,15 +1,19 @@
 #!/usr/bin/python3
-from sqlalchemy import Column, Integer, String, text
+"""
+State class
+"""
+
+from sqlalchemy import Column, Integer, String
 from sqlalchemy.orm import relationship
 from sqlalchemy.ext.declarative import declarative_base
-"""
-    Module that performs creates a States class based off of Base.
-"""
 
 Base = declarative_base()
+
+
 class State(Base):
+    """State class"""
     __tablename__ = 'states'
+
     id = Column(Integer, primary_key=True)
     name = Column(String(128), nullable=False)
-
     cities = relationship("City", backref="state", cascade="all, delete")
